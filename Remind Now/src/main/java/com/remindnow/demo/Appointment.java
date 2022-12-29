9d9f0986fefc22d6992fd9e1f66b67b8a702761e
Aidan Chadha <98668078+Aidan7757@users.noreply.github.com>
2022-12-29 14:19:15 -0500

    Add files via upload

---
+++ Remind Now/src/main/java/com/remindnow/demo/Appointment.java
@@ -0,0 +1,20 @@
+package com.remindnow.demo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Appointment {
+
+    @JsonProperty("name")
+    public String name;
+    
+    @JsonProperty("date")
+    public String date;
+
+    @JsonProperty("time")
+    public String time;
+    
+    @JsonProperty("phoneNumber")
+    public String phoneNumber;
+
+}
+ 
